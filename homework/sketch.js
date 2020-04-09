5e112de4609e0ed5ce41ed054d98263499d8f9f0
jacob-andrews <60157726+jacob-2005@users.noreply.github.com>
2020-04-09 11:49:06 -0500

    homwork change 2

--- homework/sketch.js
+++ homework/sketch.js
@@ -1,12 +1,7 @@
 
 let w = 10;
-// An array of 0s and 1s
 let cells;
-
- // We arbitrarily start with just the middle cell having a state of "1"
 let generation = 0;
-
-// An array to store the ruleset, for example {0,1,1,0,1,1,0,1}
 let ruleset = [0, 1, 0, 1, 1, 0, 1, 0];
 
 function setup() {
@@ -34,35 +29,27 @@ function draw() {
   }
 }
 
-// The process of creating the new generation
+
 function generate() {
-  // First we create an empty array for the new values
   let nextgen = Array(cells.length);
-  // For every spot, determine new state by examing current state, and neighbor states
-  // Ignore edges that only have one neighor
   for (let i = 1; i < cells.length-1; i++) {
-    let left   = cells[i-1];   // Left neighbor state
-    let me     = cells[i];     // Current state
-    let right  = cells[i+1];   // Right neighbor state
-    nextgen[i] = rules(left, me, right); // Compute next generation state based on ruleset
+    let left   = cells[i-1];
+    let me     = cells[i];
+    let right  = cells[i+1];
+    nextgen[i] = rules(left, me, right); 
   }
-  // The current generation is the new generation
   cells = nextgen;
   generation++;
 }
 
-
-// Implementing the Wolfram rules
-// Could be improved and made more concise, but here we can explicitly
-//see what is going on for each case
 function rules(a, b, c) {
-  if (a == 0 && b == 0 && c == 0) return ruleset[0];
-  if (a == 0 && b == 0 && c == 1) return ruleset[1];
-  if (a == 0 && b == 1 && c == 0) return ruleset[2];
-  if (a == 0 && b == 1 && c == 1) return ruleset[3];
-  if (a == 1 && b == 0 && c == 0) return ruleset[4];
-  if (a == 1 && b == 0 && c == 1) return ruleset[5];
-  if (a == 1 && b == 1 && c == 0) return ruleset[6];
-  if (a == 1 && b == 1 && c == 1) return ruleset[7];
+  if (a == 1 && b == 1 && c == 1) return ruleset[0];
+  if (a == 1 && b == 1 && c == 0) return ruleset[1];
+  if (a == 1 && b == 0 && c == 1) return ruleset[2];
+  if (a == 1 && b == 0 && c == 0) return ruleset[3];
+  if (a == 0 && b == 1 && c == 1) return ruleset[4];
+  if (a == 0 && b == 1 && c == 0) return ruleset[5];
+  if (a == 0 && b == 0 && c == 1) return ruleset[6];
+  if (a == 0 && b == 0 && c == 0) return ruleset[7];
   return 0;
 }
